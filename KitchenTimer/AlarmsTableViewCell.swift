55f59ab510f1466988358e687600cdc9dc1c20d7
mani <mani.sedighi@visibacare.com>
2019-01-29 10:47:53 +0100

    first commit

---
+++ KitchenTimer/AlarmsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AlarmsTableViewCell.swift
+//  KitchenTimer
+//
+//  Created by Mani Sedighi on 2019-01-29.
+//  Copyright © 2019 Mani Sedighi. All rights reserved.
+//
+
+import UIKit
+
+class AlarmsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
